880116e21c2cb12d7dbae4456bdf50e09d1f40f4
PpBb94 <68030154@kmitl.ac.th>
2025-06-16 13:46:35 +0000

    do02-00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Prompt the user to enter a number
+try:
+    num_str = input("Enter a number: ")
+    num = int(num_str)  # Convert the input string to an integer
+
+    # Check if the number is equal to zero
+    if num == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
+except EOFError:
+    print("\nInput cancelled.")
